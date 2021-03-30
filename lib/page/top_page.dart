4888b0b65672d854557cc2cf12ef6baa2d213bf2
Yoshiki <s4shiki.dev@gmail.com>
2021-03-30 09:15:13 +0900

    ルール一覧を固定テキストで表示

--- lib/page/top_page.dart
+++ lib/page/top_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// トップ画面（ルール一覧）
+// トップ画面
 class TopPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,11 +15,32 @@ class TopPage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(child: Text('ルールを一覧表示するよ')),
+      // body: Center(child: Text('ルールを一覧表示するよ')),
+      body: RuleListWidget(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => print('ルール追加画面に遷移するよ'),
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+// ルール一覧
+class RuleListWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> rules = [
+      'ビールが欲しくなったら => 炭酸水を飲む',
+      '食べたくなったら => ナッツを食べる',
+      'タバコが吸いたくなったら => ニコレスを吸う'
+    ];
+
+    return ListView.builder(
+      itemCount: rules.length, // リスト数を与える
+      itemBuilder: (context, index) {
+        // 表示内容を返す。itemCount分だけ繰り返される。indexは0からカウントアップする
+        return Text(rules[index]);
+      },
     );
   }
 }
